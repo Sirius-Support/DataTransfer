345096e44835fbfecff9fb37ad767606b9c31bfb
ericburdon <eric.burdon0105@yahoo.com>
2020-04-07 20:40:28 +0800

    data-transfer local api-url for now

--- DataTransfer/TransferModule.cs
+++ DataTransfer/TransferModule.cs
@@ -2,12 +2,18 @@
 using System.IO;
 using System.Timers;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace DataTransfer
 {
     public class TransferModule
     {
         public static string MAIN_DIR = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "remote-work");
+        public static string SCREENSHOTS_DIR = Path.Combine(MAIN_DIR, "screenshots");
+        public static string ACTIVITY_DIR = Path.Combine(MAIN_DIR, "activity");
+        public static string CAM_DIR = Path.Combine(MAIN_DIR, "cam");
+        public static string API_URL = "http://localhost:5000/api/rwk";
 
         Timer timer;
         TransferSettings setting;
@@ -32,22 +38,67 @@ namespace DataTransfer
             timer.Interval = setting.DATA_TRANSFER_CYCLE * 1000;/* ///// * 60; */
             timer.Elapsed += Timer_Tick;            
         }
-        private void Timer_Tick(object sender, EventArgs e)
+        private async void Timer_Tick(object sender, EventArgs e)
         {
             try
             {
-                List<string> files = new List<string>(Directory.EnumerateFiles(MAIN_DIR));
+                List<string> files = new List<string>(Directory.EnumerateFiles(SCREENSHOTS_DIR));
                 foreach (string file in files)
                 {
                     if (file.EndsWith(".rwk"))
                     {
-                        Console.WriteLine(file);
+                        await Upload("screenshots", file);
+                        File.Delete(file);
+                        Console.WriteLine("Screen-shot file '" + file + "' sent.");
+                    }
+                }
+
+                files = new List<string>(Directory.EnumerateFiles(ACTIVITY_DIR));
+                foreach (string file in files)
+                {
+                    if (file.EndsWith(".rwk"))
+                    {
+                        await Upload("activity", file);
+                        File.Delete(file);
+                        Console.WriteLine("Activity-log file '" + file + "' sent.");
+                    }
+                }
+
+                files = new List<string>(Directory.EnumerateFiles(CAM_DIR));
+                foreach (string file in files)
+                {
+                    if (file.EndsWith(".rwk"))
+                    {
+                        await Upload("cam", file);
+                        File.Delete(file);
+                        Console.WriteLine("Camera-shot file '" + file + "' sent.");
                     }
                 }
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
+            }
+        }
+        private async Task<System.IO.Stream> Upload(string type, string filePath)
+        {
+            string timestamp = Path.GetFileName(filePath);
+            
+            using (FileStream paramFileStream = File.OpenRead(filePath))
+            {
+                HttpContent fileStreamContent = new StreamContent(paramFileStream);
+                using (var client = new HttpClient())
+                using (var formData = new MultipartFormDataContent())
+                {
+                    formData.Add(fileStreamContent, "captureFile", "" + setting.USER_ID + "^" + type + "^" + timestamp);
+                    var response = await client.PostAsync(API_URL + "/capture", formData);
+                    if (!response.IsSuccessStatusCode)
+                    {
+                        return null;
+                    }
+
+                    return await response.Content.ReadAsStreamAsync();
+                }
             }
         }
     }
--- DataTransfer/TransferSettings.cs
+++ DataTransfer/TransferSettings.cs
@@ -5,18 +5,20 @@ namespace DataTransfer
 {
     public class TransferSettings
     {
+        public int USER_ID { get; set; } = 0;
         public byte DATA_TRANSFER_CYCLE { get; set; } = 15;     // in minute
 
         public static TransferSettings readSettings()
         {
-            var confFileName = Path.Combine(TransferModule.MAIN_DIR, "tconf.cnf");
+            var confFileName = "config.dat";
             TransferSettings ret = new TransferSettings();
 
             try
             {
                 byte[] fileBytes = File.ReadAllBytes(confFileName);
                 
-                ret.DATA_TRANSFER_CYCLE = fileBytes[0];
+                ret.USER_ID = (fileBytes[0] << 24) | (fileBytes[1] << 16) | (fileBytes[2] << 8) | fileBytes[3];
+                ret.DATA_TRANSFER_CYCLE = fileBytes[14];
             }
             catch(Exception e)
             {
